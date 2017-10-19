a3f8485e00464c86e476349e9a153e73f8d480c8
Abhi Yerra <abhi@berkeley.edu>
2017-10-19 20:12:06 +0000

    Include certs

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 sed -i "s/SOCKET_SERVER/${SOCKET_SERVER}/g" /etc/nginx/nginx.conf
+sed -i "s/FULL_CERT/${FULL_CERT}/g" /etc/nginx/nginx.conf
+sed -i "s/PRIVATE_KEY/${PRIVATE_KEY}/g" /etc/nginx/nginx.conf
+
 nginx
